c6dab70376acb5960aff1b5926b74d5670088554
Lindley Graham <lgraham@ices.utexas.edu>
2014-03-12 19:56:48 -0500

    pre pylint draft done

--- polysim/run_framework/adaptive_sampling.py
+++ polysim/run_framework/adaptive_sampling.py
@@ -54,7 +54,7 @@ class adaptiveSamples(pickleable):
             given a step size based on the paramter domain size
         :type transition_kernel: :class:~`transition_kernel`
         :param function heuristic: functional that acts on the data used to
-            determine the ``step_size``
+            determine the proposed change to the ``step_size``
         :param string savefile: filename to save samples and data
         :rtype: tuple
         :returns: (``parameter_samples``, ``data_samples``) where
@@ -70,7 +70,7 @@ class adaptiveSamples(pickleable):
 
         # Why don't we solve the problem at initial samples?
         data_old = self.model(initial_samples)
-        heur_old = heuristic(data_old)
+        (heur_old, proposal) = heuristic.delta_step(data_old, None)
          
         # Initialize Nx1 vector Step_size = something reasonable (based on size
         # of domain and transition kernel type)
@@ -90,27 +90,11 @@ class adaptiveSamples(pickleable):
 
             # Solve the model for the samples_new.
             data_new = self.model(samples_new)
-
+            
             # Make some decision about changing step_size(k).  There are
             # multiple ways to do this.
-            # Determine proposed step size:
-            # Assume that the heuristic acts as a functional on the data so
-            # that heuristic(data).shape = (num_samples,)
-            # Evaluate heuristic for new data.
-            heur_new = heuristic(data_new)
-            heur_diff = (heur_new-heur_old)/heuristic.MAX
-            # Compare to heuristic for old data.
-            # Is the heuristic NOT close?
-            heur_close = np.logical_not(np.isclose(heur_diff, 0,
-                atol=heuristic.TOL))
-            # Is the heuristic greater/lesser?
-            heur_greater = np.logical_and(heur_diff > 0, heur_close)
-            heur_lesser = np.logical_and(heur_diff < 0, heur_close)
-
             # Determine step size
-            proposal = np.ones(heur_new.shape)
-            proposal[heur_greater] = 2.0
-            proposal[heur_lesser] = 0.5
+            (heur_old, proposal) = heuristic.delta_step(data_new, heur_old)
             step_ratio = proposal*step_ratio
             # Is the ratio greater than max?
             step_ratio[step_ratio > max_ratio] = max_ratio
@@ -127,7 +111,6 @@ class adaptiveSamples(pickleable):
 
             # samples_old = samples_new
             samples_old = samples_new
-            heur_old = heur_new
 
 
 def class transition_kernel(pickleable):
@@ -178,11 +161,172 @@ def class transition_kernel(pickleable):
         return step
 
 
-def class heuristic(pickleable):
+def class single_dist_heuristic(pickleable):
+    """
+    We assume we know the distribution rho_D on the QoI and that the goal is to
+    determine inverse regions of high probability accurately (in terms of
+    getting the measure correct). This class provides a method for determining
+    the proposed change in step size as follows. We check if the QoI at each of the
+    samples_new(k) are closer or farther away from a region of high probability
+    in D than the QoI at samples_old(k).  For example, if they are closer, then
+    we can reduce the step_size(k) by 1/2.
 
-    def __init__(self, maximum, tolerance):
+    maximum
+        maximum value of rho_D on D
+    tolerance 
+        a tolerance used to determine if two different values are close
+    increase
+        the multiple to increase the step size by
+    decrease
+        the multiple to decrease the step size by
+
+    """
+
+    def __init__(self, maximum, rho_D, tolerance =1E-08, increase=2.0, 
+            decrease=0.5):
+        """
+        Initialization
+        """
         self.MAX = maximum
         self.TOL = tolerance
+        self.rho_D = rho_D
+        self.increase = increase
+        self.decrease = decrease
+
+    def delta_step(self, data_new, heur_old = None):
+        """
+        This method determines the proposed change in step size. 
+        
+        :param data_new: QoI for a given batch of samples 
+        :type data_new: :class:`np.array` of shape (samples_per_batch, mdim)
+        :param heur_old: heuristic evaluated at previous step
+        :rtype: tuple
+        :returns: (heur_new, proposal)
+
+        """
+        # Evaluate heuristic for new data.
+        heur_new = self.rho_D(data_new)
+
+        if heur_old == None:
+            return (heur_new, None)
+        else:
+            heur_diff = (heur_new-heur_old)/self.MAX
+            # Compare to heuristic for old data.
+            # Is the heuristic NOT close?
+            heur_close = np.logical_not(np.isclose(heur_diff, 0,
+                atol=self.TOL))
+            # Is the heuristic greater/lesser?
+            heur_greater = np.logical_and(heur_diff > 0, heur_close)
+            heur_lesser = np.logical_and(heur_diff < 0, heur_close)
+
+            # Determine step size
+            proposal = np.ones(heur_new.shape)
+            proposal[heur_greater] = self.increase
+            proposal[heur_lesser] = self.decrease
+            return (heur_new, proposal)
+
+def class multi_dist_heuristic(pickleable):
+    """
+    The goal is to make a sampling that is robust to different types of
+    distributions on QoI, i.e., we do not know a priori where the regions of
+    high probability are in D. This class provides a method for determining the
+    proposed step size as follows. We keep track of the change of the QoI
+    values from one sample to the next compared to the total range of QoI
+    values explored so far. If a big relative change is detected, then you know
+    that you have come across a region with larger derivatives and you should
+    place more samples around there to resolve the induced regions of
+    generalized contours, i.e., reduce the step size. If the change in QoI
+    values is relatively small, you are in a region where there is little
+    sensitivity, so take larger step sizes.
+
+    radius
+        current estimate of the radius of D (1/2 the diameter of D)
+    mean
+        current estimate of the mean QoI
+    batch_num
+        current batch number
+    TOL 
+        a tolerance used to determine if two different values are close
+    increase
+        the multiple to increase the step size by
+    decrease
+        the multiple to decrease the step size by
+
+    """
+
+    def __init__(self, tolerance =1E-08, increase=2.0, 
+            decrease=0.5):
+        """
+        Initialization
+        """
+        self.radius = None
+        self.mean = None
+        self.batch_num = 0
+        self.TOL = tolerance
+        self.increase = increase
+        self.decrease = decrease
+
+    def reset(self):
+        """
+        Resets the the batch number and the estimates of the mean and maximum
+        distance from the mean.
+        """
+        self.radius = None
+        self.mean = None
+        self.batch_num = 0
+
+    def delta_step(self, data_new, heur_old = None):
+        """
+        This method determines the proposed change in step size. 
+        
+        :param data_new: QoI for a given batch of samples 
+        :type data_new: :class:`np.array` of shape (samples_per_batch, mdim)
+        :param heur_old: QoI evaluated at previous step
+        :rtype: tuple
+        :returns: (heur_new, proposal)
+
+        """
+        # Evaluate heuristic for new data.
+        heur_new = data_new
+        norm_l2 = np.norm(data_new, 2, 1)
+        self.batch_num = self.batch_num + 1
+
+        if heur_old == None:
+            proposal = None
+            # calculate the mean
+            self.mean = np.mean(data_new, 1)
+            # calculate the distance from the mean
+            vec_from_mean = heur_new - np.repeat([self.mean],
+                    heur_new.shape[0])
+            # estimate the radius of D
+            self.radius = np.max(np.linalg.norm(vec_from_mean, 2, 1)) 
+        else:
+            # update the estimate of the mean
+            self.mean = (self.batch_num-1)*self.mean + np.mean(data_new, 1)
+            self.mean = self.mean/self.batch_num
+            # calculate the distance from the mean
+            vec_from_mean = heur_new - np.repeat([self.mean],
+                    heur_new.shape[0])
+            # esitmate the radius of D
+            self.radius = max(np.max(np.linalg.norm(vec_from_mean, 2, 1)),
+                    self.radius)
+            # calculate the relative change in QoI
+            heur_diff = (heur_new-heur_old)
+            # normalize by the radius of D
+            heur_diff = np.linalg.norm(vec_from_mean, 2, 1)/self.radius
+            # Compare to heuristic for old data.
+            # Is the heuristic NOT close?
+            heur_close = np.logical_not(np.isclose(heur_diff, 0,
+                atol=self.TOL))
+            # Is the heuristic greater/lesser?
+            heur_greater = np.logical_and(heur_diff > 0, heur_close)
+            heur_lesser = np.logical_and(heur_diff < 0, heur_close)
+            # Determine step size
+            proposal = np.ones(heur_new.shape)
+            proposal[heur_greater] = self.decrease
+            proposal[heur_lesser] = self.increase
+        return (heur_new, proposal)
+
 
 
 
