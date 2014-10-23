03eff3e6f1ce67488117e0098d0b30ea50bd1fd5
Lindley Graham <lgraham@ices.utexas.edu>
2014-10-23 17:44:41 -0500

    added note for when number of mpi tasks exceeds number of land classes

--- polyadcirc/pyGriddata/grid_management.py
+++ polyadcirc/pyGriddata/grid_management.py
@@ -148,6 +148,11 @@ class gridInfo(pickleable):
         """
         if class_nums == None:
             class_nums = range(len(self.__landclasses))
+        if rank > class_nums:
+            print "There are more MPI TASKS than land classes."
+            print "This code only scales to MPI_TASKS = len(land_classes)."
+            print "Extra MPI TASKS will not be used."
+            return
 
         # Are there any binary files?
         binaries = glob.glob(self.basis_dir+'/*.asc.binary')
