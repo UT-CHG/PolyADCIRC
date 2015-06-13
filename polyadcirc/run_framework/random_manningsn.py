bd08bbbe0a83744a740a7adcc4ec21a4e2e18145
Lindley Graham <lichgraham@gmail.com>
2015-06-13 01:48:20 -0500

    changed for mpirun version

--- polyadcirc/run_framework/random_manningsn.py
+++ polyadcirc/run_framework/random_manningsn.py
@@ -450,6 +450,61 @@ class runSet(pickleable):
     def write_run_script_noibrun(self, num_procs, num_jobs, procs_pnode, TpN,
                          screenout=True, num_writers=None):
         """
+        MPI VERSION 1.4.1 for EUCLID with the modules needed to run ADCIRC
+
+        Creates a bash script called ``self.script_name`` in ``self.base_dir``
+        and a set of rankfiles named ``rankfile_n`` to run multiple non-interacting
+        parallel programs in parallel.
+
+        :type num_procs: int
+        :param num_procs: number of processes per job
+        :type num_jobs: int
+        :param num_jobs: number of jobs to run
+        :param int procs_pnode: number of processors per node
+        :param boolean screenout: flag (True --  write ``ADCIRC`` output to
+            screen, False -- write ``ADCIRC`` output to temp file)
+        :param int num_writers: number of MPI processes to dedicate soley to
+            the task of writing ascii files
+        :param int TpN: number of tasks (processors to use) per node (wayness)
+        :rtype: string
+        :returns: name of bash script for running a batch of jobs within our
+            processor allotment
+
+        """
+        tmp_file = self.script_name.partition('.')[0]+'.tmp'
+        num_nodes = int(math.ceil(num_procs*num_jobs/float(TpN)))
+        with open(os.path.join(self.base_dir, self.script_name), 'w') as f:
+            #f.write('#!/bin/bash\n')
+            # change i to 2*i or something like that to no use all of the
+            # processors on a node?
+            for i in xrange(num_jobs):
+                # write the bash file containing mpi commands
+                #line = 'ibrun -n {:d} -o {:d} '.format(num_procs,
+                #        num_procs*i*(procs_pnode/TpN))
+                line = 'mpirun -f $TMP/machines -binding user:'
+                # comma separated list of ranks w/o spaces
+                for j in xrange(num_procs-1):
+                    line += str(j+i*num_procs)+','
+                line += str((i+1)*num_procs)+' '
+                if TpN != procs_pnode:
+                    line += '-ranks-per-proc {:d} '.format(TpN)
+                line += ' -np {:d} '.format(num_procs)
+                line += './padcirc -I {0} -O {0} '.format(self.rf_dirs[i])
+                if num_writers:
+                    line += '-W '+str(num_writers)+' '
+                if not screenout:
+                    line += '> '+tmp_file
+                line += ' &\n'
+                f.write(line)
+            f.write('wait\n')
+        curr_stat = os.stat(self.base_dir+'/'+self.script_name)
+        os.chmod(self.base_dir+'/'+self.script_name,
+                 curr_stat.st_mode | stat.S_IXUSR)
+        return self.script_name
+
+    def write_run_script_noibrun_MPI19(self, num_procs, num_jobs, procs_pnode, TpN,
+                         screenout=True, num_writers=None):
+        """
         Creates a bash script called ``self.script_name`` in ``self.base_dir``
         and a set of rankfiles named ``rankfile_n`` to run multiple non-interacting
         parallel programs in parallel.
